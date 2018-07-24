5858b9596310a1fad300c0f9fb51715464a54f12
Josiah Maina <josiah5115@gmail.com>
2018-07-24 17:12:15 -0600

    fixed more bugs

--- stage-4/src/redux/productsController.js
+++ stage-4/src/redux/productsController.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const getAllProducts = function() {
-    return axios.get("https://practiceapi.devmountain.com/products/")
-    .then(response => response.data);
+export default {
+     getAllProducts: function() {
+        return axios.get("https://practiceapi.devmountain.com/products/")
+        .then(response => response.data);
+    }
 }
