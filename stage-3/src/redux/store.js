4171534bc455716a3450adc7f6c833a1c51f8285
Josiah Maina <josiah5115@gmail.com>
2018-07-24 16:15:39 -0600

    fixed bugs

--- stage-3/src/redux/store.js
+++ stage-3/src/redux/store.js
@@ -1,4 +1,9 @@
+// import React from 'react';
 import reducer from './reducer';
 import reduxPromiseMiddleware from 'redux-promise-middleware';
+import { createStore, applyMiddleware } from 'redux';
+
+
+// const {createStore} = Redux;
 
 export default createStore(reducer, applyMiddleware(reduxPromiseMiddleware()));
